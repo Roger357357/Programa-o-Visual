01e3ddb95ccc2835764aa646ab251c9260e0eb78
Rogerfelipe <rogerfelipe357@gmail.com>
2021-04-12 21:51:46 -0300

    Aula sobre CheckBox e RadioButton
    Formulário feito na aula junto com a professora no dia 12/04

---
+++ Aula_7/AulaCheckRadio/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace AulaCheckRadio
+{
+    partial class FrmCheckRadio
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpQualidades = new System.Windows.Forms.GroupBox();
+            this.chkInteligente = new System.Windows.Forms.CheckBox();
+            this.chkAmavel = new System.Windows.Forms.CheckBox();
+            this.chkSabio = new System.Windows.Forms.CheckBox();
+            this.chkPaciente = new System.Windows.Forms.CheckBox();
+            this.chkTolerante = new System.Windows.Forms.CheckBox();
+            this.chkModesto = new System.Windows.Forms.CheckBox();
+            this.chkTranquilo = new System.Windows.Forms.CheckBox();
+            this.chkCalmo = new System.Windows.Forms.CheckBox();
+            this.chkComunicativo = new System.Windows.Forms.CheckBox();
+            this.chkBonito = new System.Windows.Forms.CheckBox();
+            this.chkExuberante = new System.Windows.Forms.CheckBox();
+            this.chkAlegre = new System.Windows.Forms.CheckBox();
+            this.lblQualidades = new System.Windows.Forms.Label();
+            this.btnQualidades = new System.Windows.Forms.Button();
+            this.lblTodas = new System.Windows.Forms.Label();
+            this.grpCor = new System.Windows.Forms.GroupBox();
+            this.rdbAzul = new System.Windows.Forms.RadioButton();
+            this.rdbVermelho = new System.Windows.Forms.RadioButton();
+            this.rdbVerde = new System.Windows.Forms.RadioButton();
+            this.grpQualidades.SuspendLayout();
+            this.grpCor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpQualidades
+            // 
+            this.grpQualidades.Controls.Add(this.chkAlegre);
+            this.grpQualidades.Controls.Add(this.chkExuberante);
+            this.grpQualidades.Controls.Add(this.chkBonito);
+            this.grpQualidades.Controls.Add(this.chkComunicativo);
+            this.grpQualidades.Controls.Add(this.chkCalmo);
+            this.grpQualidades.Controls.Add(this.chkTranquilo);
+            this.grpQualidades.Controls.Add(this.chkModesto);
+            this.grpQualidades.Controls.Add(this.chkTolerante);
+            this.grpQualidades.Controls.Add(this.chkPaciente);
+            this.grpQualidades.Controls.Add(this.chkSabio);
+            this.grpQualidades.Controls.Add(this.chkAmavel);
+            this.grpQualidades.Controls.Add(this.chkInteligente);
+            this.grpQualidades.Location = new System.Drawing.Point(12, 12);
+            this.grpQualidades.Name = "grpQualidades";
+            this.grpQualidades.Size = new System.Drawing.Size(329, 226);
+            this.grpQualidades.TabIndex = 0;
+            this.grpQualidades.TabStop = false;
+            this.grpQualidades.Text = "Qualidades";
+            // 
+            // chkInteligente
+            // 
+            this.chkInteligente.AutoSize = true;
+            this.chkInteligente.Location = new System.Drawing.Point(6, 36);
+            this.chkInteligente.Name = "chkInteligente";
+            this.chkInteligente.Size = new System.Drawing.Size(103, 24);
+            this.chkInteligente.TabIndex = 0;
+            this.chkInteligente.Text = "Inteligente";
+            this.chkInteligente.UseVisualStyleBackColor = true;
+            this.chkInteligente.CheckedChanged += new System.EventHandler(this.chkInteligente_CheckedChanged);
+            // 
+            // chkAmavel
+            // 
+            this.chkAmavel.AutoSize = true;
+            this.chkAmavel.Location = new System.Drawing.Point(6, 66);
+            this.chkAmavel.Name = "chkAmavel";
+            this.chkAmavel.Size = new System.Drawing.Size(80, 24);
+            this.chkAmavel.TabIndex = 1;
+            this.chkAmavel.Text = "Amável";
+            this.chkAmavel.UseVisualStyleBackColor = true;
+            this.chkAmavel.CheckedChanged += new System.EventHandler(this.chkAmavel_CheckedChanged);
+            // 
+            // chkSabio
+            // 
+            this.chkSabio.AutoSize = true;
+            this.chkSabio.Location = new System.Drawing.Point(6, 96);
+            this.chkSabio.Name = "chkSabio";
+            this.chkSabio.Size = new System.Drawing.Size(69, 24);
+            this.chkSabio.TabIndex = 2;
+            this.chkSabio.Text = "Sábio";
+            this.chkSabio.UseVisualStyleBackColor = true;
+            this.chkSabio.CheckedChanged += new System.EventHandler(this.chkSabio_CheckedChanged);
+            // 
+            // chkPaciente
+            // 
+            this.chkPaciente.AutoSize = true;
+            this.chkPaciente.Location = new System.Drawing.Point(6, 126);
+            this.chkPaciente.Name = "chkPaciente";
+            this.chkPaciente.Size = new System.Drawing.Size(90, 24);
+            this.chkPaciente.TabIndex = 3;
+            this.chkPaciente.Text = "Paciente";
+            this.chkPaciente.UseVisualStyleBackColor = true;
+            this.chkPaciente.CheckedChanged += new System.EventHandler(this.chkPaciente_CheckedChanged);
+            // 
+            // chkTolerante
+            // 
+            this.chkTolerante.AutoSize = true;
+            this.chkTolerante.Location = new System.Drawing.Point(6, 156);
+            this.chkTolerante.Name = "chkTolerante";
+            this.chkTolerante.Size = new System.Drawing.Size(95, 24);
+            this.chkTolerante.TabIndex = 4;
+            this.chkTolerante.Text = "Tolerante";
+            this.chkTolerante.UseVisualStyleBackColor = true;
+            this.chkTolerante.CheckedChanged += new System.EventHandler(this.chkTolerante_CheckedChanged);
+            // 
+            // chkModesto
+            // 
+            this.chkModesto.AutoSize = true;
+            this.chkModesto.Location = new System.Drawing.Point(6, 186);
+            this.chkModesto.Name = "chkModesto";
+            this.chkModesto.Size = new System.Drawing.Size(90, 24);
+            this.chkModesto.TabIndex = 5;
+            this.chkModesto.Text = "Modesto";
+            this.chkModesto.UseVisualStyleBackColor = true;
+            this.chkModesto.CheckedChanged += new System.EventHandler(this.chkModesto_CheckedChanged);
+            // 
+            // chkTranquilo
+            // 
+            this.chkTranquilo.AutoSize = true;
+            this.chkTranquilo.Location = new System.Drawing.Point(181, 36);
+            this.chkTranquilo.Name = "chkTranquilo";
+            this.chkTranquilo.Size = new System.Drawing.Size(93, 24);
+            this.chkTranquilo.TabIndex = 1;
+            this.chkTranquilo.Text = "Tranquilo";
+            this.chkTranquilo.UseVisualStyleBackColor = true;
+            this.chkTranquilo.CheckedChanged += new System.EventHandler(this.chkTranquilo_CheckedChanged);
+            // 
+            // chkCalmo
+            // 
+            this.chkCalmo.AutoSize = true;
+            this.chkCalmo.Location = new System.Drawing.Point(181, 66);
+            this.chkCalmo.Name = "chkCalmo";
+            this.chkCalmo.Size = new System.Drawing.Size(73, 24);
+            this.chkCalmo.TabIndex = 6;
+            this.chkCalmo.Text = "Calmo";
+            this.chkCalmo.UseVisualStyleBackColor = true;
+            this.chkCalmo.CheckedChanged += new System.EventHandler(this.chkCalmo_CheckedChanged);
+            // 
+            // chkComunicativo
+            // 
+            this.chkComunicativo.AutoSize = true;
+            this.chkComunicativo.Location = new System.Drawing.Point(181, 96);
+            this.chkComunicativo.Name = "chkComunicativo";
+            this.chkComunicativo.Size = new System.Drawing.Size(123, 24);
+            this.chkComunicativo.TabIndex = 7;
+            this.chkComunicativo.Text = "Comunicativo";
+            this.chkComunicativo.UseVisualStyleBackColor = true;
+            this.chkComunicativo.CheckedChanged += new System.EventHandler(this.chkComunicativo_CheckedChanged);
+            // 
+            // chkBonito
+            // 
+            this.chkBonito.AutoSize = true;
+            this.chkBonito.Location = new System.Drawing.Point(180, 156);
+            this.chkBonito.Name = "chkBonito";
+            this.chkBonito.Size = new System.Drawing.Size(74, 24);
+            this.chkBonito.TabIndex = 8;
+            this.chkBonito.Text = "Bonito";
+            this.chkBonito.UseVisualStyleBackColor = true;
+            this.chkBonito.CheckedChanged += new System.EventHandler(this.chkBonito_CheckedChanged);
+            // 
+            // chkExuberante
+            // 
+            this.chkExuberante.AutoSize = true;
+            this.chkExuberante.Location = new System.Drawing.Point(180, 126);
+            this.chkExuberante.Name = "chkExuberante";
+            this.chkExuberante.Size = new System.Drawing.Size(110, 24);
+            this.chkExuberante.TabIndex = 9;
+            this.chkExuberante.Text = "Exuberante";
+            this.chkExuberante.UseVisualStyleBackColor = true;
+            this.chkExuberante.CheckedChanged += new System.EventHandler(this.chkExuberante_CheckedChanged);
+            // 
+            // chkAlegre
+            // 
+            this.chkAlegre.AutoSize = true;
+            this.chkAlegre.Location = new System.Drawing.Point(181, 186);
+            this.chkAlegre.Name = "chkAlegre";
+            this.chkAlegre.Size = new System.Drawing.Size(74, 24);
+            this.chkAlegre.TabIndex = 10;
+            this.chkAlegre.Text = "Alegre";
+            this.chkAlegre.UseVisualStyleBackColor = true;
+            this.chkAlegre.CheckedChanged += new System.EventHandler(this.chkAlegre_CheckedChanged);
+            // 
+            // lblQualidades
+            // 
+            this.lblQualidades.AutoSize = true;
+            this.lblQualidades.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQualidades.Location = new System.Drawing.Point(14, 253);
+            this.lblQualidades.Name = "lblQualidades";
+            this.lblQualidades.Size = new System.Drawing.Size(215, 20);
+            this.lblQualidades.TabIndex = 1;
+            this.lblQualidades.Text = "Nenhum item selecionado";
+            // 
+            // btnQualidades
+            // 
+            this.btnQualidades.Location = new System.Drawing.Point(107, 364);
+            this.btnQualidades.Name = "btnQualidades";
+            this.btnQualidades.Size = new System.Drawing.Size(101, 36);
+            this.btnQualidades.TabIndex = 2;
+            this.btnQualidades.Text = "Qualidades";
+            this.btnQualidades.UseVisualStyleBackColor = true;
+            this.btnQualidades.Click += new System.EventHandler(this.btnQualidades_Click);
+            // 
+            // lblTodas
+            // 
+            this.lblTodas.AutoSize = true;
+            this.lblTodas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTodas.Location = new System.Drawing.Point(14, 282);
+            this.lblTodas.MaximumSize = new System.Drawing.Size(329, 0);
+            this.lblTodas.Name = "lblTodas";
+            this.lblTodas.Size = new System.Drawing.Size(68, 20);
+            this.lblTodas.TabIndex = 3;
+            this.lblTodas.Text = "Todas: ";
+            // 
+            // grpCor
+            // 
+            this.grpCor.Controls.Add(this.rdbVerde);
+            this.grpCor.Controls.Add(this.rdbVermelho);
+            this.grpCor.Controls.Add(this.rdbAzul);
+            this.grpCor.Location = new System.Drawing.Point(380, 12);
+            this.grpCor.Name = "grpCor";
+            this.grpCor.Size = new System.Drawing.Size(223, 225);
+            this.grpCor.TabIndex = 4;
+            this.grpCor.TabStop = false;
+            this.grpCor.Text = "Cor Favorita";
+            // 
+            // rdbAzul
+            // 
+            this.rdbAzul.AutoSize = true;
+            this.rdbAzul.Location = new System.Drawing.Point(20, 44);
+            this.rdbAzul.Name = "rdbAzul";
+            this.rdbAzul.Size = new System.Drawing.Size(58, 24);
+            this.rdbAzul.TabIndex = 0;
+            this.rdbAzul.TabStop = true;
+            this.rdbAzul.Text = "Azul";
+            this.rdbAzul.UseVisualStyleBackColor = true;
+            this.rdbAzul.CheckedChanged += new System.EventHandler(this.rdbAzul_CheckedChanged);
+            // 
+            // rdbVermelho
+            // 
+            this.rdbVermelho.AutoSize = true;
+            this.rdbVermelho.Location = new System.Drawing.Point(20, 85);
+            this.rdbVermelho.Name = "rdbVermelho";
+            this.rdbVermelho.Size = new System.Drawing.Size(95, 24);
+            this.rdbVermelho.TabIndex = 1;
+            this.rdbVermelho.TabStop = true;
+            this.rdbVermelho.Text = "Vermelho";
+            this.rdbVermelho.UseVisualStyleBackColor = true;
+            this.rdbVermelho.CheckedChanged += new System.EventHandler(this.rdbVermelho_CheckedChanged);
+            // 
+            // rdbVerde
+            // 
+            this.rdbVerde.AutoSize = true;
+            this.rdbVerde.Location = new System.Drawing.Point(20, 126);
+            this.rdbVerde.Name = "rdbVerde";
+            this.rdbVerde.Size = new System.Drawing.Size(70, 24);
+            this.rdbVerde.TabIndex = 2;
+            this.rdbVerde.TabStop = true;
+            this.rdbVerde.Text = "Verde";
+            this.rdbVerde.UseVisualStyleBackColor = true;
+            this.rdbVerde.CheckedChanged += new System.EventHandler(this.rdbVerde_CheckedChanged);
+            // 
+            // FrmCheckRadio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 412);
+            this.Controls.Add(this.grpCor);
+            this.Controls.Add(this.lblTodas);
+            this.Controls.Add(this.btnQualidades);
+            this.Controls.Add(this.lblQualidades);
+            this.Controls.Add(this.grpQualidades);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmCheckRadio";
+            this.Text = "Exemplo com Check e Radio";
+            this.Load += new System.EventHandler(this.FrmCheckRadio_Load);
+            this.grpQualidades.ResumeLayout(false);
+            this.grpQualidades.PerformLayout();
+            this.grpCor.ResumeLayout(false);
+            this.grpCor.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpQualidades;
+        private System.Windows.Forms.CheckBox chkAlegre;
+        private System.Windows.Forms.CheckBox chkExuberante;
+        private System.Windows.Forms.CheckBox chkBonito;
+        private System.Windows.Forms.CheckBox chkComunicativo;
+        private System.Windows.Forms.CheckBox chkCalmo;
+        private System.Windows.Forms.CheckBox chkTranquilo;
+        private System.Windows.Forms.CheckBox chkModesto;
+        private System.Windows.Forms.CheckBox chkTolerante;
+        private System.Windows.Forms.CheckBox chkPaciente;
+        private System.Windows.Forms.CheckBox chkSabio;
+        private System.Windows.Forms.CheckBox chkAmavel;
+        private System.Windows.Forms.CheckBox chkInteligente;
+        private System.Windows.Forms.Label lblQualidades;
+        private System.Windows.Forms.Button btnQualidades;
+        private System.Windows.Forms.Label lblTodas;
+        private System.Windows.Forms.GroupBox grpCor;
+        private System.Windows.Forms.RadioButton rdbVerde;
+        private System.Windows.Forms.RadioButton rdbVermelho;
+        private System.Windows.Forms.RadioButton rdbAzul;
+    }
+}
+
---
+++ Aula_7/AulaCheckRadio/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaCheckRadio
+{
+    public partial class FrmCheckRadio : Form
+    {
+        public FrmCheckRadio()
+        {
+            InitializeComponent();
+        }
+
+        private void chkInteligente_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkInteligente.Checked)
+            {
+                lblQualidades.Text = chkInteligente.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+            
+        }
+
+        private void chkAmavel_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkAmavel.Checked)
+            {
+                lblQualidades.Text = chkAmavel.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+        }
+
+        private void chkSabio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkSabio.Checked)
+            {
+                lblQualidades.Text = chkSabio.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+        }
+
+        private void chkPaciente_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkPaciente.Checked)
+            {
+                lblQualidades.Text = chkPaciente.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+        }
+
+        private void chkTolerante_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkTolerante.Checked)
+            {
+                lblQualidades.Text = chkTolerante.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+        }
+
+        private void chkModesto_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkModesto.Checked)
+            {
+                lblQualidades.Text = chkModesto.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+        }
+
+        private void chkTranquilo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkTranquilo.Checked)
+            {
+                lblQualidades.Text = chkTranquilo.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+        }
+
+        private void chkCalmo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkCalmo.Checked)
+            {
+                lblQualidades.Text = chkCalmo.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+        }
+
+        private void chkComunicativo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkComunicativo.Checked)
+            {
+                lblQualidades.Text = chkComunicativo.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+        }
+
+        private void chkExuberante_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkExuberante.Checked)
+            {
+                lblQualidades.Text = chkExuberante.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+        }
+
+        private void chkBonito_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkBonito.Checked)
+            {
+                lblQualidades.Text = chkBonito.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+        }
+
+        private void chkAlegre_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkAlegre.Checked)
+            {
+                lblQualidades.Text = chkAlegre.Text;
+            }
+            else
+            {
+                lblQualidades.Text = "Nenhuma item selecionado";
+            }
+        }
+
+        private void btnQualidades_Click(object sender, EventArgs e)
+        {
+            lblTodas.Text = "Todas: ";
+            if (chkInteligente.Checked)
+            {
+                lblTodas.Text += chkInteligente.Text + " ";
+            }
+            if (chkAmavel.Checked)
+            {
+                lblTodas.Text += chkAmavel.Text + " ";
+            }
+            if (chkSabio.Checked)
+            {
+                lblTodas.Text += chkSabio.Text + " ";
+            }
+            if (chkPaciente.Checked)
+            {
+                lblTodas.Text += chkPaciente.Text + " ";
+            }
+            if (chkTolerante.Checked)
+            {
+                lblTodas.Text += chkTolerante.Text + " ";
+            }
+            if (chkModesto.Checked)
+            {
+                lblTodas.Text += chkModesto.Text + " ";
+            }
+            if (chkTranquilo.Checked)
+            {
+                lblTodas.Text += chkTranquilo.Text + " ";
+            }
+            if (chkCalmo.Checked)
+            {
+                lblTodas.Text += chkCalmo.Text + " ";
+            }
+            if (chkComunicativo.Checked)
+            {
+                lblTodas.Text += chkComunicativo.Text + " ";
+            }
+            if (chkExuberante.Checked)
+            {
+                lblTodas.Text += chkExuberante.Text + " ";
+            }
+            if (chkBonito.Checked)
+            {
+                lblTodas.Text += chkBonito.Text + " ";
+            }
+            if (chkAlegre.Checked)
+            {
+                lblTodas.Text += chkAlegre.Text + " ";
+            }
+
+        }
+
+        private void rdbAzul_CheckedChanged(object sender, EventArgs e)
+        {
+            grpCor.BackColor = Color.Blue;
+        }
+
+        private void rdbVermelho_CheckedChanged(object sender, EventArgs e)
+        {
+            grpCor.BackColor = Color.Red;
+        }
+
+        private void rdbVerde_CheckedChanged(object sender, EventArgs e)
+        {
+            grpCor.BackColor = Color.Green;
+        }
+
+        private void FrmCheckRadio_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Boa Noite", "Boas Vindas", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Hand);
+        }
+    }
+}
